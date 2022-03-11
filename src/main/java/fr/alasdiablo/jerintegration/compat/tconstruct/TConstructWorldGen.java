de707ba9ff5ff8d633f1831d725cdc57db5e1930
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2022-03-11 10:47:54 +0100

    build: add source of TConstruct

---
+++ src/main/java/fr/alasdiablo/jerintegration/compat/tconstruct/TConstructWorldGen.java
@@ -0,0 +1,10 @@
+package fr.alasdiablo.jerintegration.compat.tconstruct;
+
+import fr.alasdiablo.jerintegration.api.WorldGenIntegration;
+import jeresources.api.IWorldGenRegistry;
+
+public class TConstructWorldGen extends WorldGenIntegration {
+    @Override
+    public void registerWorldGen(IWorldGenRegistry registry) {
+    }
+}
