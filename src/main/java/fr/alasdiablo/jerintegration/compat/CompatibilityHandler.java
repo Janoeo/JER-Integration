45b571af427d52e8d232be9204f6986cca2cd61a
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2022-04-09 11:21:20 +0200

    feat: back-port Immersive Patch

--- src/main/java/fr/alasdiablo/jerintegration/compat/CompatibilityHandler.java
+++ src/main/java/fr/alasdiablo/jerintegration/compat/CompatibilityHandler.java
@@ -4,7 +4,7 @@ import fr.alasdiablo.jerintegration.JERIntegration;
 import fr.alasdiablo.jerintegration.api.IJERIntegration;
 import fr.alasdiablo.jerintegration.compat.ae2.AppEngWorldGen;
 import fr.alasdiablo.jerintegration.compat.create.CreateWorldGen;
-import fr.alasdiablo.jerintegration.compat.minecraft.MinecraftWorldGen;
+import fr.alasdiablo.jerintegration.compat.immersiveengineering.ImmersiveEngineeringWorldGen;
 import jeresources.api.IJERAPI;
 import jeresources.compatibility.JERAPI;
 
@@ -17,9 +17,10 @@ public class CompatibilityHandler {
 
     public CompatibilityHandler() {
         this.compatibilityPatch = new ArrayList<>();
+    }
 
+    public void preInit() {
         JERIntegration.LOGGER.info("Applying Minecraft patch");
-        this.compatibilityPatch.add(new MinecraftWorldGen());
         if (JERIntegration.Compat.AE2) {
             try {
                 JERIntegration.LOGGER.info("Applying Applied Energistics 2 patch");
@@ -34,6 +35,14 @@ public class CompatibilityHandler {
                 this.compatibilityPatch.add(new CreateWorldGen());
             } catch (Exception e) {
                 JERIntegration.LOGGER.info("Failing to apply patch for Create");
+            }
+        }
+        if (JERIntegration.Compat.IMMERSIVE_ENGINEERING) {
+            try {
+                JERIntegration.LOGGER.info("Applying Immersive Engineering patch");
+                this.compatibilityPatch.add(new ImmersiveEngineeringWorldGen());
+            } catch (Exception e) {
+                JERIntegration.LOGGER.info("Failing to apply patch for Immersive Engineering");
             }
         }
     }
